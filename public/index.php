42f9a764ff1efedb956e0bd8e938043b673e5df7
MenshikovMakxim <147256792+MenshikovMakxim@users.noreply.github.com>
2024-05-04 17:31:26 +0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+
+    $homeController = new Me\Task7\Controllers\HomeController();
+    $aboutController = new Me\Task7\Controllers\AboutController();
+    $contactsController = new Me\Task7\Controllers\ContactsController();
+    $catalogueController = new Me\Task7\Controllers\CatalogueController();
+
+    $r->addRoute('GET', '/', [$homeController, 'index']);
+    $r->addRoute('GET', '/home', [$homeController, 'index']);
+    $r->addRoute('GET', '/about', [$aboutController, 'index']);
+    $r->addRoute('GET', '/contacts', [$contactsController, 'index']);
+    $r->addRoute('GET', '/catalogue',[$catalogueController,'index']);
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        header('Location: /');
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        header('Location: /');
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
